1b5a96cbb012e3a999f260eaac549df43a3eb065
JaneStack <mrumunjanet@gmail.com>
2021-10-31 21:29:59 -0700

    Update forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -35,7 +35,7 @@ const forecast = (latitude, longitude, callback) => {
 
             callback('Unable to find location')
         } else { 
-            callback(undefined, `${body.current.weather_descriptions[0]}.It is currently ${body.current.temperature} degreess out. It feels like ${body.current.feelslike} degrees out`)
+            callback(undefined, `${body.current.weather_descriptions[0]}.It is currently ${body.current.temperature} degreess out. It feels like ${body.current.feelslike} degrees out,.  With a humidity of ${body.current.humidity}%`)
     } 
     })
 }
